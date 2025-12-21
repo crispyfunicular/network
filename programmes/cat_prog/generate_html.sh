f12def223a1f82670ac0315abae59252b6e39cd4
Morgane Bona-Pellissier <morgane@bona-pellissier.net>
2025-12-21 18:45:58 +0100

    séparation de l'ancien script en deux parties
    Il y a maintenant deux scripts :
    - un pour récupérer les métadonnées de chaque URL et les mettre dans un tsv
    - un autre pour générer le fichier html

--- programmes/cat_prog/generate_html.sh
+++ programmes/cat_prog/generate_html.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# ./miniprojet-3.sh ../urls/fr.txt > ../tableaux/tableau-fr.html
+# ./generate_html.sh ../tableaux/URLs.tsv > ../tableaux/tableau_cat.html
 
 # $# number of arguments passed to the program
-if [ $# -ne 1 ]
-then
-	echo "ce programme demande un argument"
-	exit 1
-fi
+#if [ $# -ne 1 ]
+#then
+#	echo "ce programme demande un argument"
+#	exit 1
+#fi
 
-# $1 first argument passed to the program
-URL=$1
+# $1 first argument passed to the program; "tableaux/URLs.tsv" by default
+tsv=${1-tableaux/URLs.tsv}
 
 # cat << EOF allows to print multiple lines at once
 # Write the begining of the HTML file/table
@@ -21,12 +21,12 @@ cat << EOF
 			<meta charset="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/versions/bulma-no-dark-mode.min.css">
-			<title>Mini-projet 2</title>
+			<title>La vie des mots sur le net</title>
 		</head>
 
 		<body>
             <section class="section">
-                <div class="container">
+                <div class="tabme-container">
 					<table class="table is-hoverable is-bordered is-striped">
 						
 						<thead class=""has-background-primary-35 has-text-primary-35-invert"">
@@ -48,45 +48,15 @@ cat << EOF
 						<tbody>
 EOF
 
-# counter for the line number of the urls file
-lineno=0
+
 while read -r line;
 do
 	# increment the line counter by 1	
-	lineno=$(expr $lineno + 1)
-
-    # curl: make an http request
-	# -s --silent (do not display extra metadata)
-	# -I --head (metadata only)
-	# -L --location (follow redirections)
-	# -w --write-out "format string" (add metadata at the end of the standard output)
-	# tail -n 1 (keep only the last line)
-	# $metadata is the HTTP response's status code and contains, separated by a tabulation, the content type
-	metadata=$(curl -s -I -L -w "%{response_code}\t%{content_type}" -o /dev/null $line)
-	
-	# Keep only the first part of the variable, tab being the default separator
-	# $(...) -> mandatory to store the result of the command into the variable
-	response_code=$(echo "$metadata" | cut -f1)
-	
-	# Keep the part after the tabulation and look for an expression with "charset",
-	# and divide it in two parts in the "=" sign and keep only the right part ("UTF-8")
-	charset=$(echo "$metadata" | cut -f2 | grep -E -o "charset=.*" | cut -d= -f2)
-
-
-	# ok if the http response code starts with 2 (200, 201,...)
-	ok=$(echo $response_code | grep ^2)
-	
-
-	if [ -n "$ok" ];
-	then
-		# lynx: browse a web page
-		# -dump (disable interactive mode)
-		# -nolist (remove links)
-		# wc -w (count the number of words)
-		num_words=$(lynx -dump -nolist $line | wc -w)
-	else
-		num_words=0
-	fi
+	lineno=$(echo "$line" | cut -f 1)
+	url=$(echo "$line" | cut -f 2)
+	response_code=$(echo "$line" | cut -f 3)
+	charset=$(echo "$line" | cut -f 4)
+	num_words=$(echo "$line" | cut -f 5)
 	
 
 	# Write one table row (<tr>) for each line in fr.txt
@@ -99,9 +69,9 @@ do
 							<td>$lineno</td>
 							<td><a href="$line">$line</td>
 							<td${rc_class}>$response_code</td>
-							<td>$charset</td>
-							<td>$num_words</td>
-							<td>occurrences</td>
+							<td></td>
+							<td></td>
+							<td></td>
 							<td></td>
 							<td></td>
 							<td></td>
@@ -128,23 +98,8 @@ EOF
 EOF
 	fi
 
-	cat << EOF
-							<tr>
-								<td>$lineno</td>
-								<td><a href="$line">$line</td>
-								<td${rc_class}>$response_code</td>
-								<td>$charset</td>
-								<td>$num_words</td>
-								<td>occurrences</td>
-								<td><a href="">page HTML brute</a></td>
-								<td><a href="">dump textuel</a></td>
-								<td><a href="">concordancier HTML</a></td>
-								<td><a href="">robots.txt</a></td>
-								<td><a href="">concordancier couleurs</a></td>
-							</tr>
-EOF
 
-done < $URL
+done < $tsv
 
 # Write the end of the HTML table/file
 cat << EOF
---
+++ programmes/cat_prog/get_URLs_cat.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# ./miniprojet-3.sh ../urls/fr.txt > ../tableaux/tableau-fr.html
+
+# $# number of arguments passed to the program
+if [ $# -ne 1 ]
+then
+	echo "ce programme demande un argument"
+	exit 1
+fi
+
+# $1 first argument passed to the program
+URL=$1
+
+# ***
+
+# counter for the line number of the urls file
+lineno=0
+while read -r line;
+do
+	# display each line in the stderr
+	echo "Fetching $line" 1>&2
+
+	# increment the line counter by 1	
+	lineno=$(expr $lineno + 1)
+
+    # curl: make an http request
+	# -s --silent (do not display extra metadata)
+	# -I --head (metadata only)
+	# -L --location (follow redirections)
+	# -w --write-out "format string" (add metadata at the end of the standard output)
+	# tail -n 1 (keep only the last line)
+	# $metadata is the HTTP response's status code and contains, separated by a tabulation, the content type
+	metadata=$(curl -s -I -L -w "%{response_code}\t%{content_type}" -o /dev/null $line)
+	
+	# Keep only the first part of the variable, tab being the default separator
+	# $(...) -> mandatory to store the result of the command into the variable
+	response_code=$(echo "$metadata" | cut -f1)
+	
+	# Keep the part after the tabulation and look for an expression with "charset",
+	# and divide it in two parts in the "=" sign and keep only the right part ("UTF-8")
+	charset=$(echo "$metadata" | cut -f2 | grep -E -o "charset=.*" | cut -d= -f2)
+
+
+	# ok if the http response code starts with 2 (200, 201,...)
+	ok=$(echo $response_code | grep ^2)
+	
+
+	if [ -n "$ok" ];
+	then
+		# lynx: browse a web page
+		# -dump (disable interactive mode)
+		# -nolist (remove links)
+		# wc -w (count the number of words)
+		num_words=$(lynx -dump -nolist $line | wc -w)
+	else
+		num_words=0
+	fi
+
+	echo -e "$lineno\t$line\t$response_code\t$charset\t$num_words"
+
+done < $URL
